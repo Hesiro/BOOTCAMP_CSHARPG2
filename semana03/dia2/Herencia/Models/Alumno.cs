cb7b2260d84e041f64babb76d723b8af83938ef1
Henry Sifuentes <henrywsr@outlook.com>
2024-06-19 19:58:51 -0500

    Actualizacion

---
+++ semana03/dia2/Herencia/Models/Alumno.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Herencia.Models
+{
+    internal class Alumno:Persona
+    {
+        public int Nota { get; set; }
+
+        public override void Mostrar()
+        {
+            base.Mostrar();
+            Console.WriteLine($"Tienes una nota de {Nota}");
+        }
+    }
+}
---
+++ semana03/dia2/Herencia/Program.cs
@@ -0,0 +1,24 @@
+﻿using Herencia.Models;
+
+namespace Herencia
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola, Henry!");
+
+            Alumno alumno = new Alumno();
+            alumno.Nombre = "Henry Sifuentes";
+            alumno.Email = "henrywsr@gmail.com";
+            alumno.Nota = 17;
+            alumno.Mostrar();
+            alumno.MostrarNota();
+
+            Profesor profesor = new Profesor();
+            profesor.Nombre = "Cesar Mayta";
+            profesor.Email = "cmayta@gmail.com";
+            profesor.Mostrar();
+        }
+    }
+}
