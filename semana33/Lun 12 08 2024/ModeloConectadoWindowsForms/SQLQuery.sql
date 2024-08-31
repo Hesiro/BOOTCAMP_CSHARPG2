be075fbaf390a0a3a504ea81d1d7ade97ddda7bc
Henry Sifuentes <henrywsr@outlook.com>
2024-08-31 12:17:10 -0500

    Actualizacion

--- semana33/Lun 12 08 2024/ModeloConectadoWindowsForms/SQLQuery.sql
+++ semana33/Lun 12 08 2024/ModeloConectadoWindowsForms/SQLQuery.sql
@@ -13,12 +13,26 @@ create table Users
 	Name varchar(150),
 	Email varchar(150),
 	Address varchar(150),
+	Age int,
 	Id_Flight int,
 	constraint fk_Users_Flight foreign key (Id_Flight) references Flight(Id)
 );
 
 insert into Flight(Airline, FlightNumber, Destination, AirPlaneType)
 values 
-	('American Airlines', 'AA100', 'New York', 'Boeing 737'),
+	('American Airlines', 'AA120', 'New York', 'Boeing 777'),
 	('Delta Airlines', 'DL200', 'Los Angeles', 'Airbus A320'),
 	('United Airlines', 'UA300', 'Chicago','Boeing 777');
+
+select * from Flight
+
+insert into Users (Name, Email, Address, Age, Id_Flight)
+values
+	('Henry', 'henrywsr@outlook.com', 'AAHH Cerrro Atarraya E8', 40, 1),
+	('Irene', 'iromisoncco@gmail.com', 'Urb. San Pedro', 59, 2),
+	('Liz', 'lizmasiro@gmail.com', 'AAHH Cerrro Atarraya E8', 33, 1),
+	('David', 'david@outlook.com', 'Urb. San Pedro', 64, 2);
+
+select * from Users;
+
+--drop table Users
---
+++ semana33/Mie 14 08 2024/ModeloDesconectadoWpfApp/ModeloDesconectadoWpfApp/MainWindow.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ModeloDesconectadoWpfApp
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<string> listAircrafts = new List<string>();
+        DataSet1 dataSet1 = new DataSet1();
+
+        /*Flight*/
+        DataSet1TableAdapters.FlightTableAdapter flightTableAdapter = 
+            new DataSet1TableAdapters.FlightTableAdapter();
+        DataSet1.FlightDataTable flightDataTable = 
+            new DataSet1.FlightDataTable();
+
+        /*User*/
+        DataSet1TableAdapters.UsersTableAdapter usersTableAdapter =
+            new DataSet1TableAdapters.UsersTableAdapter();
+        DataSet1.UsersDataTable userDataTable = 
+            new DataSet1.UsersDataTable();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            listAircrafts.Clear();
+            listAircrafts.Add("Boeing 737");
+            listAircrafts.Add("Boeing 747");
+            listAircrafts.Add("Boeing 757");
+            listAircrafts.Add("Boeing 767");
+            listAircrafts.Add("Boeing 777");
+            listAircrafts.Add("Boeing 120");
+
+            comboBoxAircraft.ItemsSource = listAircrafts;
+
+            RefreshDataGridFhlight();
+        }
+
+        private void RefreshDataGridFhlight()
+        {
+            flightTableAdapter.Fill(dataSet1.Flight);
+            flightTableAdapter.Fill(flightDataTable);
+
+            dataGridFlights.ItemsSource = flightDataTable;
+
+            comboBoxFlights.ItemsSource = flightDataTable;
+            comboBoxFlights.DisplayMemberPath = "FlightNumber";
+            comboBoxFlights.SelectedValuePath = "Id";
+
+            comboBoxNextFlight.ItemsSource = flightDataTable;
+            comboBoxNextFlight.DisplayMemberPath = "FlightNumber";
+            comboBoxNextFlight.SelectedValuePath = "Id";
+        }
+
+        private void comboBoxFlights_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
