2353b726331b268c820ddd80f4ecf4ffba398a89
Henry Sifuentes <henrywsr@outlook.com>
2024-09-06 23:00:08 -0500

    Actualizacion

---
+++ semana36/Mie 04 09 2024/DemoCleanArchitecture/InfraestructureLayer/Implementations/EmployeeRepository.cs
@@ -0,0 +1,61 @@
+﻿using ApplicationLayer.DTOs;
+using ApplicationLayer.Interfaces;
+using DomainLayer.Entities;
+using InfraestructureLayer.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InfraestructureLayer.Implementations
+{
+    public class EmployeeRepository : IEmployee
+    {
+        readonly AppDbContext _dbContext;
+
+        public EmployeeRepository(AppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public async Task<ServiceResponse> AddAsync(Employee employee)
+        {
+            var response = await _dbContext.Employees.FirstOrDefaultAsync(x => x.Name.ToLower() == employee.Name.ToLower());
+            if (response != null) {
+                return new ServiceResponse(false, "El empleado ya existe");
+            }
+            _dbContext.Employees.Add(employee);
+            await _dbContext.SaveChangesAsync();
+            return new ServiceResponse(true, "Empleado guardado exitosamente");
+        }
+
+        public async Task<ServiceResponse> DeleteAsync(int id)
+        {
+            var response = await _dbContext.Employees.FindAsync(id);
+            if (response == null) {
+                return new ServiceResponse(false, "No se encontro el empleado a eliminar");
+            }
+            _dbContext.Employees.Remove(response);
+            await _dbContext.SaveChangesAsync();
+            return new ServiceResponse(true, "Empleado eliminado exitosamente");
+        }
+
+        public async Task<List<Employee>> GetAllAsync()
+        {
+            return await _dbContext.Employees.AsNoTracking().ToListAsync();
+        }
+
+        public async Task<Employee> GetByIdAsync(int id)
+        {
+            return await _dbContext.Employees.FindAsync(id);
+        }
+
+        public async Task<ServiceResponse> UpdateAsync(Employee employee)
+        {
+            _dbContext.Employees.Update(employee);
+            await _dbContext.SaveChangesAsync(true);
+            return new ServiceResponse(true, "Actualizado exitosamente");
+        }
+    }
+}
---
+++ semana36/Mie 04 09 2024/DemoCleanArchitecture/WebAPI/Controllers/EmployeesController.cs
@@ -0,0 +1,57 @@
+﻿using ApplicationLayer.Interfaces;
+using DomainLayer.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+        readonly IEmployee _employee;
+
+        public EmployeesController(IEmployee employee)
+        {
+            _employee = employee;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var employeesList = await _employee.GetAllAsync();
+            return Ok(employeesList);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var employee = await _employee.GetByIdAsync(id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+            return Ok(employee);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddEmployee(Employee employee)
+        {
+            var response = await _employee.AddAsync(employee);
+            return Ok(response);
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> DeleteEmployee(int id)
+        {
+            var response = await _employee.DeleteAsync(id);
+            return Ok(response);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateEmployee(Employee employee) { 
+            var response = await _employee.UpdateAsync(employee);
+            return Ok(response);
+        }
+    }
+}
